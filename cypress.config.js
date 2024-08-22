8787538cb0beaf947025459c6836634c27b8eca8
sri <sri@sris-MBP.attlocal.net>
2024-08-22 11:55:17 -0500

    Use greptags

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/grep/src/plugin')(config);
       allureWriter(on, config);
+      return config;
     },
   },
 });
